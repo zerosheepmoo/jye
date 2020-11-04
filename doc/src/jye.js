08194af55b78c6b4236a03cb846c19a9781094c3
zerosheepmoo <youngyang@Youngs-MacBook-Pro.local>
2020-11-05 03:44:20 +0900

    init

---
+++ doc/src/jye.js
@@ -0,0 +1,43 @@
+let action = function (value, o) {
+    if (value.includes("정예은")) {
+        o.innerHTML = `<pre>
+________  __            __          __      __                      __  __ 
+/        |/  |          /  |        /  |    /  |                    /  |/  |
+$$$$$$$$/ $$/   ______  $$ |____   _$$ |_   $$/  _______    ______  $$ |$$ |
+$$ |__    /  | /      \\ $$      \\ / $$   |  /  |/       \\  /      \\ $$ |$$ |
+$$    |   $$ |/$$$$$$  |$$$$$$$  |$$$$$$/   $$ |$$$$$$$  |/$$$$$$  |$$ |$$ |
+$$$$$/    $$ |$$ |  $$ |$$ |  $$ |  $$ | __ $$ |$$ |  $$ |$$ |  $$ |$$/ $$/ 
+$$ |      $$ |$$ \\__$$ |$$ |  $$ |  $$ |/  |$$ |$$ |  $$ |$$ \\__$$ | __  __ 
+$$ |      $$ |$$    $$ |$$ |  $$ |  $$  $$/ $$ |$$ |  $$ |$$    $$ |/  |/  |
+$$/       $$/  $$$$$$$ |$$/   $$/    $$$$/  $$/ $$/   $$/  $$$$$$$ |$$/ $$/ 
+              /  \\__$$ |                                  /  \\__$$ |        
+              $$    $$/                                   $$    $$/         
+               $$$$$$/                                     $$$$$$/          
+        </pre>`
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    let jyeCon = document.getElementById("jye");
+
+    let t = document.createElement("input");
+    t.type = "text"
+    let o = document.createElement("div");
+
+    let b = document.createElement("button");
+    b.textContent = "Confirm";
+    t.addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            action(t.value, o);
+        }
+    });
+    b.addEventListener("click", function(event) {
+        action(t.value, o);
+    })
+    jyeCon.appendChild(t);
+    t.parentElement.appendChild(b);
+    t.parentElement.appendChild(o);
+    o.className = "jye-output";
+    b.className = "jye-button";
+    t.className = "jye-input";
+});
