ec0c01fc2e99969ed1d96d85bcf348d9fcf2d856
zerosheepmoo <youngyang@Youngs-MacBook-Pro.local>
2021-01-12 16:46:28 +0900

    enter

--- docs/src/jye.js
+++ docs/src/jye.js
@@ -4,13 +4,20 @@ let action = function (value, o) {
         let m = t.getMonth();
         let d = t.getDate();
         if (m === 0 && d === 12) {
-            o.innerHTML = `<pre style="font-size: 0.5em";>
-db   db  .d8b.  d8888b. d8888b. db    db      d8888b. d888888b d8888b. d888888b db   db d8888b.  .d8b.  db    db db 
-88   88 d8' \`8b 88  \`8D 88  \`8D \`8b  d8'      88  \`8D   \`88'   88  \`8D \`~~88~~' 88   88 88  \`8D d8' \`8b \`8b  d8' 88 
-88ooo88 88ooo88 88oodD' 88oodD'  \`8bd8'       88oooY'    88    88oobY'    88    88ooo88 88   88 88ooo88  \`8bd8'  YP 
-88~~~88 88~~~88 88~~~   88~~~      88         88~~~b.    88    88\`8b      88    88~~~88 88   88 88~~~88    88       
-88   88 88   88 88      88         88         88   8D   .88.   88 \`88.    88    88   88 88  .8D 88   88    88    db 
-YP   YP YP   YP 88      88         YP         Y8888P' Y888888P 88   YD    YP    YP   YP Y8888D' YP   YP    YP    YP
+            o.innerHTML = `<pre style="font-size: 0.3em";>
+db   db  .d8b.  d8888b. d8888b. db    db     
+88   88 d8' \`8b 88  \`8D 88  \`8D \`8b  d8'
+88ooo88 88ooo88 88oodD' 88oodD'  \`8bd8'    
+88~~~88 88~~~88 88~~~   88~~~      88         
+88   88 88   88 88      88         88        
+YP   YP YP   YP 88      88         YP       
+
+d8888b. d888888b d8888b. d888888b db   db d8888b.  .d8b.  db    db db
+88  \`8D   \`88'   88  \`8D \`~~88~~' 88   88 88  \`8D d8' \`8b \`8b  d8' 88 
+88oooY'    88    88oobY'    88    88ooo88 88   88 88ooo88  \`8bd8'  YP 
+88~~~b.    88    88\`8b      88    88~~~88 88   88 88~~~88    88     
+88   8D   .88.   88 \`88.    88    88   88 88  .8D 88   88    88    db
+Y8888P' Y888888P 88   YD    YP    YP   YP Y8888D' YP   YP    YP    YP
 
                     (\`-')  _ 
               .->    ( OO).-/ 
