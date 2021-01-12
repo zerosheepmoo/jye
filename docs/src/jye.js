9243c0c846f2c7598beab91068ee33ad5bf26c4b
zerosheepmoo <youngyang@Youngs-MacBook-Pro.local>
2021-01-12 16:38:18 +0900

    birthaday

--- docs/src/jye.js
+++ docs/src/jye.js
@@ -1,6 +1,31 @@
 let action = function (value, o) {
     if (value.includes("정예은")) {
-        o.innerHTML = `<pre style="font-size: 7px;">
+        let t = new Date();
+        let m = t.getMonth();
+        let d = t.getDate();
+        if (m === 0 && d === 12) {
+            o.innerHTML = `<pre style="font-size: 1em";>
+db   db  .d8b.  d8888b. d8888b. db    db      d8888b. d888888b d8888b. d888888b db   db d8888b.  .d8b.  db    db db 
+88   88 d8' \`8b 88  \`8D 88  \`8D \`8b  d8'      88  \`8D   \`88'   88  \`8D \`~~88~~' 88   88 88  \`8D d8' \`8b \`8b  d8' 88 
+88ooo88 88ooo88 88oodD' 88oodD'  \`8bd8'       88oooY'    88    88oobY'    88    88ooo88 88   88 88ooo88  \`8bd8'  YP 
+88~~~88 88~~~88 88~~~   88~~~      88         88~~~b.    88    88\`8b      88    88~~~88 88   88 88~~~88    88       
+88   88 88   88 88      88         88         88   8D   .88.   88 \`88.    88    88   88 88  .8D 88   88    88    db 
+YP   YP YP   YP 88      88         YP         Y8888P' Y888888P 88   YD    YP    YP   YP Y8888D' YP   YP    YP    YP
+
+                    (\`-')  _ 
+              .->    ( OO).-/ 
+   <-.--. ,--.'  ,-.(,------. 
+ (\`-'| ,|(\`-')'.'  / |  .---' 
+ (OO |(_|(OO \     / (|  '--.  
+,--. |  | |  /   /)  |  .--'  
+|  '-'  / \`-/   /\`   |  \`---. 
+\`-----'    \`--'     \`------' 
+
+</pre>
+`
+        }
+        else {
+            o.innerHTML = `<pre style="font-size: 7px;">
 ________  __            __          __      __                      __  __ 
 /        |/  |          /  |        /  |    /  |                    /  |/  |
 $$$$$$$$/ $$/   ______  $$ |____   _$$ |_   $$/  _______    ______  $$ |$$ |
@@ -14,6 +39,7 @@ $$/       $$/  $$$$$$$ |$$/   $$/    $$$$/  $$/ $$/   $$/  $$$$$$$ |$$/ $$/
               $$    $$/                                   $$    $$/         
                $$$$$$/                                     $$$$$$/          
         </pre>`
+        }
     }
     else {
         o.innerHTML =""
